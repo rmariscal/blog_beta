bac30289454bb60cd29ad2169c89f179643107e4
rmariscal <rmariscalparedes@gmail.com>
2023-06-11 20:00:17 -0600

    ok

--- beta_blog_useful_files.R
+++ beta_blog_useful_files.R
@@ -1,3 +1,5 @@
 #https://beamilz.com/posts/2022-06-05-creating-a-blog-with-quarto/en/
 
 #https://quarto.org/docs/websites/website-blog.html
+
+# https://github.com/djnavarro/quarto-blog/blob/main/_quarto.yml
